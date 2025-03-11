e4f486626ab46377d4b17b33058af3edc128855f
maruf010 <mhbijoy013@gmail.com>
2025-03-11 22:48:26 +0600

    card design added

--- index.js
+++ index.js
@@ -33,15 +33,28 @@ const displayVideos = (videos) => {
         // console.log(video)
         const videoCard = document.createElement('div');
         videoCard.innerHTML = `
-        <div class="card bg-base-100 shadow-sm">
-            <figure>
-                <img class="w-full h-56" src="${video.thumbnail}" alt="Shoes"/>
+        <div class="card bg-base-100">
+            <figure class="relative">
+                <img class="w-full h-56 rounded-lg object-cover" src="${video.thumbnail}" alt="Shoes" />
+                <span class="absolute bottom-2 right-2 bg-black text-white px-3 py-1 rounded-md text-sm">3hrs 56 min
+                    ago</span>
             </figure>
-            <div class="card-body">
-                <h2 class="card-title">${video.title}</h2>
-                <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-                <div class="card-actions justify-end">
-                    <button class="btn btn-primary">Buy Now</button>
+            <div class="flex py-5">
+                <div class="profile">
+                    <div class="avatar">
+                        <div class="ring-primary ring-offset-base-100 w-10 rounded-full ring ring-offset-2">
+                            <img class="" src="${video.authors[0].profile_picture}" />
+                        </div>
+                    </div>
+                </div>
+                <div class="details pl-5">
+                    <h1 class="card-title mb-2">Midnight Serenade</h1>
+                    <p class="name text-gray-500 text-sm flex mb-2">
+                        ${video.authors[0].profile_name}
+                        <img class="w-5 h-5 ml-2" src="https://img.icons8.com/?size=96&id=98A4yZTt9abw&format=png"
+                            alt="">
+                    </p>
+                    <p class="views text-gray-500 text-sm">${video.others.views} views</p>
                 </div>
             </div>
         </div>
